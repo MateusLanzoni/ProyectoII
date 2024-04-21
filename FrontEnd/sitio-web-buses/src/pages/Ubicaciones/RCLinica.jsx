b0578c62513200a2fdd4279d44d3d9bc582e86b4
Mateus Trejos <mateus.lanzoni@eia.edu.co>
2024-04-21 17:27:05 -0500

    Making the marker appear

--- FrontEnd/sitio-web-buses/src/pages/Ubicaciones/RCLinica.jsx
+++ FrontEnd/sitio-web-buses/src/pages/Ubicaciones/RCLinica.jsx
@@ -11,11 +11,15 @@ import {
 
 function RCLinica () {
   const positon = {lat: 6.2476, lng: -75.5658}
+  const EIA = {lat:6.1566, lng: -75.5176}
   return (
     <APIProvider apiKey="AIzaSyAqSAPSOBWQXZrrtLjy1PJSd7ud8wnjjXM">
-    <div className='rutaClinica'>
-      <Map zoom={11} center={positon}></Map>
-    </div>
+      <div className='rutaClinica'>
+        <Map zoom={11} center={positon}>
+          <AdvancedMarker position={EIA}></AdvancedMarker>
+          <AdvancedMarker position={positon}></AdvancedMarker>
+       </Map>
+      </div>
     </APIProvider>
   )
 }
