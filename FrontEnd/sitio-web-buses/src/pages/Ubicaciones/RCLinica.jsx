f75ab6767868a63f7a3b3ae5f0521f3f0b82516a
Mateus Trejos <mateus.lanzoni@eia.edu.co>
2024-05-20 17:49:45 -0500

    ya acabamos

--- FrontEnd/sitio-web-buses/src/pages/Ubicaciones/RCLinica.jsx
+++ FrontEnd/sitio-web-buses/src/pages/Ubicaciones/RCLinica.jsx
@@ -26,6 +26,16 @@ function RCLinica() {
         center: { lat: 6.2476, lng: -75.5658 },
       });
 
+      const directionsService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer({
+        map: map,
+        polylineOptions: {
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 6
+        }
+      });
+
       const rutaClinicaStops = [
         { lat: 6.211745, lng: -75.594203, title: "Clínica Las Américas" },
         { lat: 6.238489, lng: -75.603002, title: "Glorieta Santa Gema" },
@@ -53,20 +63,46 @@ function RCLinica() {
       strokeWeight: 2
     }); 
     clinicaPath.setMap(map);
+
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+    };
+
+    const calculateAndDisplayRoute = (directionsService, directionsRenderer) => {
+      const waypts = [
+        { lat: 6.211745, lng: -75.594203, stopover: true },
+        { lat: 6.238489, lng: -75.603002, stopover: true },
+        { lat: 6.239123, lng: -75.590589, stopover: true },
+        { lat: 6.237100, lng: -75.569929, stopover: true },
+        { lat: 6.195436, lng: -75.547296, stopover: true },
+        { lat: 6.171274, lng: -75.546824, stopover: true },
+        { lat: 6.153486, lng: -75.532917, stopover: true },
+        { lat: 6.156926, lng: -75.518177, stopover: true },
+      ];
+
+      directionsService.route({
+        origin: { lat: 6.211745, lng: -75.594203 },
+        destination: { lat:  6.156926, lng: -75.518177 },
+        waypoints: waypts,
+        travelMode: google.maps.TravelMode.DRIVING
+      }, (response, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsRenderer.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
     };
 
     loadGoogleMapsScript();
 
     // Clean up the script tag on component unmount
     return () => {
-      const scripts = document.querySelectorAll('script');
-      for (let script of scripts) {
-        if (script.src.includes('maps.googleapis.com')) {
-          script.remove();
+      document.querySelectorAll('script').forEach(script => {
+        if (script.src.includes('maps.googleapis.com') && script.onload) {
+          script.onload = null;
         }
-      }
+      });
     };
-    
   }, []);
   return <div ref={mapRef} className="rutaClinica" />;
 }
